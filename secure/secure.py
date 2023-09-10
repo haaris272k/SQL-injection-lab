c648e8eee0f4d5dcdc304c864aa5bee9feb7f829
Haaris <89451392+haaris272k@users.noreply.github.com>
2023-09-10 11:56:45 +0530

    add: logic to automate the creation of table in db

--- secure/secure.py
+++ secure/secure.py
@@ -4,7 +4,7 @@ import psycopg2
 import re
 
 # read the config file
-config_file = "config.json"
+config_file = "secure/config.json"
 with open(config_file) as f:
     config = json.load(f)
 
@@ -14,6 +14,63 @@ db_user = config.get("DB_USER")
 db_password = config.get("DB_PASSWORD")
 db_host = config.get("DB_HOST")
 table_name = config.get("TABLE_NAME")
+
+
+def create_table():
+    """
+    Automates the first time creation of table in database.
+    """
+    try:
+        # Establish a connection to the PostgreSQL server without specifying a database
+        conn = psycopg2.connect(host=db_host, user=db_user, password=db_password)
+
+        # Create a cursor object to execute SQL queries
+        cursor = conn.cursor()
+
+        # Check if the database exists
+        cursor.execute(f"SELECT 1 FROM pg_database WHERE datname = %s", (db_name,))
+        exists = cursor.fetchone()
+
+        if not exists:
+            # Database doesn't exist; ask the user to create it manually
+            print(f"Please create the database named {db_name} as it doesn't exist.")
+            return False
+        else:
+            try:
+                # Establish a connection to the newly created database
+                conn = psycopg2.connect(
+                    host=db_host, database=db_name, user=db_user, password=db_password
+                )
+
+                # Create a cursor object to execute SQL queries
+                cursor = conn.cursor()
+
+                # Check if the table exists
+                cursor.execute(f"SELECT to_regclass(%s)", (table_name,))
+                table_exists = cursor.fetchone()
+
+                if not table_exists[0]:
+                    # Create the table if it doesn't exist
+                    cursor.execute(
+                        f"CREATE TABLE {table_name} (id serial PRIMARY KEY, username VARCHAR, password VARCHAR)"
+                    )
+
+                    # Commit the changes to the database
+                    conn.commit()
+
+                    print(f"Table {table_name} created successfully.")
+                else:
+                    print(f"Table {table_name} already exists.")
+
+                # Close the cursor and the connection
+                cursor.close()
+                conn.close()
+
+            except psycopg2.Error as e:
+                print(f"\n\033[91mError creating table: {e}\033[0m")
+
+    except psycopg2.Error as e:
+        print(f"\n\033[91mError connecting to PostgreSQL: {e}\033[0m")
 
 
 def validate_username(username):
@@ -141,7 +198,7 @@ def register(username, password):
         cursor = conn.cursor()
 
         # Execute the query with parameters to prevent SQL injection
-        query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+        query = f"INSERT INTO {table_name} (username, password) VALUES (%s, %s)"
         cursor.execute(query, (username, password))  # security: Parameterized query
 
         # Commit the changes to the database
@@ -199,6 +256,9 @@ def app():
     else:
         print("\033[91mInvalid choice.\033[0m")
 
+
+# Call the function to create the database and table
+create_table()
 
 # Run the app
 app()
